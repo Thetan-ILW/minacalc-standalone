6df6dfc9391845516800132b70aed6937cbda01d
semyon422 <semyon422@users.noreply.github.com>
2025-05-26 23:03:34 +0500

    Add build script for VS 2022

---
+++ build.bat
@@ -0,0 +1,8 @@
+@echo off
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+cl /nologo /MD /O2 /std:c++20 -c -FoMinaCalc.obj MinaCalc/MinaCalc.cpp -DSTANDALONE_CALC
+cl /nologo /MD /O2 /std:c++20 -c -FoAPI.obj API.cpp -DSTANDALONE_CALC
+link -dll -out:minacalc.dll MinaCalc.obj API.obj /EXPORT:calc_version /EXPORT:create_calc /EXPORT:destroy_calc /EXPORT:calc_msd /EXPORT:calc_ssr
+
+pause
